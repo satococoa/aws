08a0338f4b8efee316059df1d3ea4e72f1d7fe2e
Satoshi Ebisawa <e.satoshi@gmail.com>
2011-02-27 12:58:53 +0900

    bucketの一覧と作成

--- app.rb
+++ app.rb
@@ -1,12 +1,32 @@
+# coding: utf-8
 configure do
   config = YAML::load_file('config.yml')
   ACCESS = config['aws']['access']
   SECRET = config['aws']['secret']
-  BUCKET = config['s3']['bucket']
-  THUMBS_BUCKET = config['s3']['thumbs']
+
+  S3 = RightAws::S3.new(ACCESS, SECRET)
 end
 
 get '/' do
   haml :index
 end
 
+# bucketの操作
+get '/buckets' do
+  @buckets = S3.buckets
+  haml :buckets
+end
+
+# 新しいbucketの作成
+post '/buckets' do
+  @errors = []
+  begin
+    bucket = S3.bucket(params[:bucket], true)
+    redirect '/buckets'
+  rescue RightAws::AwsError => e
+    @errors << e.message
+    @buckets = S3.buckets
+    haml :buckets
+  end
+end
+  
