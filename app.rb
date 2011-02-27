9d4c0655ff0df52f42659f1b2a36f85aaf305fcb
Satoshi Ebisawa <e.satoshi@gmail.com>
2011-02-27 12:26:41 +0900

    setup

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 configure do
   config = YAML::load_file('config.yml')
+  ACCESS = config['aws']['access']
+  SECRET = config['aws']['secret']
+  BUCKET = config['s3']['bucket']
+  THUMBS_BUCKET = config['s3']['thumbs']
 end
 
 get '/' do
+  haml :index
 end
+
